f7b85b80f2c7f01d33aeaae1eca47c42292b1c9f
slava <lobikova12@mail.ru>
2021-02-26 12:48:00 +0300

    feat: add link in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -22,6 +22,7 @@ const Footer = () => {
                 2021 year
             </div>
             <div>
+
                 <a href="https://www.youtube.com/watch?v=YYWCCQxTUwA">
                     Link to youtube
                 </a>
